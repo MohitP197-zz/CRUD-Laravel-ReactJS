3d69f10954de020151cd61f452f644c58f509657
MohitP197 <paudel823@gmail.com>
2019-02-20 19:06:04 +0545

    Updated react component App.js

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-export default class Example extends Component {
+export default class App extends Component {
     render() {
         return (
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                         <div className="card">
-                            <div className="card-header">Example Component</div>
-
+                            <div className="card-header">Create Task</div>
                             <div className="card-body">
-                                I'm an example component!
+                                <form>
+                                    <div className="form-group">
+                                        <textarea
+                                            className="form-control"
+                                            rows="5"
+                                            placeholder="Create a new task"
+                                            required
+                                        />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary">
+                                        Create Task
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </div>
